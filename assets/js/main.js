23abb84f4f88418686d548d067ee792ee62c7824
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-03-12 19:06:08 -0700

    fix navbar issue and remove unneded scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,21 +141,4 @@
 
 
 
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
-
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
-
 })()
